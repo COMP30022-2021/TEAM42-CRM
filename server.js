07f94d73f94f4b1dfdac8460103ebd73fe325037
zelinwang528 <zelinw3@student.unimelb.edu.au>
2021-09-09 12:06:58 +0800

    feat_zelin: fix deployment conflict on react and backend
    Change-Id: I1cce9a516ba9224cfeeb0d6dc524ca2fd08f664b

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require("express")
 const app = express()
 
+const path = require('path');
+
+const port = process.env.PORT || 5000;
+
 // define routes:
 app.get("/", function (req, res) {
     res.send("<h1>Hello The Team!</h1>")
 })
 
+
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static('crm/build'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "crm", "build", "index.html"));
+    });
+}
+
 // start the server listening for requests
-// app.listen(process.env.PORT || 8888,
-//     () => console.log("Server is running..."));
-app.set('port', (process.env.PORT || 8081))
-app.use(express.static(path.resolve(__dirname, '../crm/build')));
+app.listen(port, (err) => {
+    if (err) return console.log(err);
+    console.log("Server is running on port: ", port)
+});
+
+
+
+
+
+// app.set('port', (process.env.PORT || 8081)
+// app.use(express.static(path.resolve(__dirname, '../crm/build')));

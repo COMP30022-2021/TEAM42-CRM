569fcc6c329a34c6905a1342d338b91f1625b7d9
zelinwang528 <zelinw3@student.unimelb.edu.au>
2021-09-09 13:02:14 +0800

    feat_zelin: fix deployment
    Change-Id: Iac24b95c6251be2a52dfdc117dfd38bc91d9f470

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # TEAM42-CRM
+
+Start the project locally:
+
+1.in ./Team42-CRM, run: npm install
+
+2.in ./Team42-CRM/crm, run: npm install
+
+3.in ./Team42-CRM, run: npm run dev
+
+
+
+then,
+
+react will run on localhost:3000, 
+
+backend will run on localhost:5000.
+
--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require("express")
 const app = express()
 
+const path = require('path');
+
+const port = process.env.PORT || 5000;
+
 // define routes:
-app.get("/", function (req, res) {
+app.get("/api", function (req, res) {
     res.send("<h1>Hello The Team!</h1>")
 })
 
+
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static('crm/build'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "crm", "build", "index.html"));
+    });
+}
+
 // start the server listening for requests
-app.listen(process.env.PORT || 8888,
-    () => console.log("Server is running..."));
+app.listen(port, (err) => {
+    if (err) return console.log(err);
+    console.log("Server is running on port: ", port)
+});

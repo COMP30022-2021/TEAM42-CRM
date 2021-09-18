aff0b4581db059cdd5360636ca7f5e632b39d21a
Hamza Ahmed Qureshi <qureshih@student.unimelb.edu.au>
2021-09-18 12:12:40 +1000

    Redirecting to login page

--- crm/src/App.js
+++ crm/src/App.js
@@ -1,24 +1,29 @@
 import React from "react";
 import "./App.css";
 import ContactPage from "./pages/ContactPage";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Header from "./Components/SignIns/Header";
-import SignUpPage from "./Components/SignIns/SignUpPage";
 import ChangePassword from "./Components/SignIns/ChangePassword";
+import Login from "./Components/SignIns/Login";
 
 function App() {
+  const [loggedIn, setLogIn] = React.useState(true);
   return (
-    <div className="App" >
-      {/*  
-      <Route exact path="/contacts" component={ContactPage} /> 
-      <Route exact path="/" component={Dashboard} />
-      <ChangePassword/>
-      <SignUpPage/>
-      */}
-      <SignUpPage/>  
+    <div className="App">
+      <Route
+        exact
+        path="/contacts"
+        render={() => render(loggedIn, ContactPage)}
+      />
+      <Route exact path="/" render={() => render(loggedIn, Dashboard)} />
+      <Route exact path="/login" component={Login} />
     </div>
   );
 }
+
+const render = ({ loggedIn, component }) => {
+  return loggedIn ? <component /> : <Redirect to="/login" />;
+};
 
 export default App;

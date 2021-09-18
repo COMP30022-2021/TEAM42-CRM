64f78a1f89ede0ba60681f4205280c21343e7873
VKASPERKOVIT <vkasperkovit@student.unimelb.edu.au>
2021-09-18 11:27:08 +1000

    New version including a Change Password and SignUpPage

--- crm/src/App.js
+++ crm/src/App.js
@@ -3,12 +3,20 @@ import "./App.css";
 import ContactPage from "./pages/ContactPage";
 import { Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import Header from "./Components/SignIns/Header";
+import SignUpPage from "./Components/SignIns/SignUpPage";
+import ChangePassword from "./Components/SignIns/ChangePassword";
 
 function App() {
   return (
-    <div className="App">
-      <Route exact path="/contacts" component={ContactPage} />
+    <div className="App" >
+      {/*  
+      <Route exact path="/contacts" component={ContactPage} /> 
       <Route exact path="/" component={Dashboard} />
+      <ChangePassword/>
+      <SignUpPage/>
+      */}
+      <SignUpPage/>  
     </div>
   );
 }

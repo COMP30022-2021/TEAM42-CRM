a6dacf211da12c149ae925e7f5e21fa076176d5d
Hamza Ahmed Qureshi <qureshih@student.unimelb.edu.au>
2021-09-26 13:40:41 +1000

    Rescaled Filter Buttons

--- crm/src/Components/Filters.js
+++ crm/src/Components/Filters.js
@@ -3,12 +3,12 @@ import React from "react";
 export default function Filters() {
   return (
     <div>
-      <btn className="button1" style={{ left: 960, top: 10 }}>
+      <div className="button1" style={{ left: "73%", top: "1.5%" }}>
         <p>Add Filter</p>
-      </btn>
-      <btn className="button1" style={{ left: 1130, top: 10 }}>
+      </div>
+      <div className="button1" style={{ left: "86%", top: "1.5%" }}>
         <p>Clear Filters</p>
-      </btn>
+      </div>
     </div>
   );
 }

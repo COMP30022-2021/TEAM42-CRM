c7c6a6b719c4449a9e7c942b6cd720d0abae4751
Hamza Ahmed Qureshi <qureshih@student.unimelb.edu.au>
2021-09-26 16:18:56 +1000

    Rescaled Dashboard

--- crm/src/Components/RecentContacts/RecentContacts.js
+++ crm/src/Components/RecentContacts/RecentContacts.js
@@ -10,46 +10,39 @@ export default function RecentContacts({ contacts }) {
         position: "absolute",
         top: "15%",
         left: "7%",
-        width: "70%",
+        width: "60%",
         height: "85%",
       }}
     >
-      <div className="block" style={{ width: "80%" }}>
-        <h1 style={{ marginLeft: 20, top: "12%" }}>Recent Contacts</h1>
+      <div className="block">
+        <h1 style={{ marginLeft: 20, top: "12%", position: "absolute" }}>
+          Recent Contacts
+        </h1>
       </div>
 
-      {contacts.map((contact, index) => (
+      {contacts.map((contact) => (
         <RecentContact contact={contact} />
       ))}
-      {contacts.map((contact, index) => (
+      {contacts.map((contact) => (
         <RecentContact contact={contact} />
       ))}
 
       <Link to="/contacts">
-        <btn
-          className="button1"
-          style={{ left: 0, bottom: 10, width: "20%", height: 50 }}
-        >
+        <div className="button4" style={{ left: "0%", bottom: 10 }}>
           <p>Customers</p>
-        </btn>
+        </div>
       </Link>
 
       <Link to="/contacts">
-        <btn
-          className="button1"
-          style={{ left: "30%", bottom: 10, width: "20%", height: 50 }}
-        >
+        <div className="button1" style={{ left: "30%", bottom: 10 }}>
           <p>Employees</p>
-        </btn>
+        </div>
       </Link>
 
       <Link to="/contacts">
-        <btn
-          className="button1"
-          style={{ left: "60%", bottom: 10, width: "20%", height: 50 }}
-        >
+        <div className="button1" style={{ left: "55%", bottom: 10 }}>
           <p>External Vendors</p>
-        </btn>
+        </div>
       </Link>
     </div>
   );

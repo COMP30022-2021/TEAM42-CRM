ec364318790b9ee32175a12a164b587b572a3908
Hamza Qureshi <54708299+qureshi-h@users.noreply.github.com>
2021-09-29 23:08:23 +1000

    changed address to email

--- models/contact.js
+++ models/contact.js
@@ -1,27 +1,23 @@
-const mysql = require('../config/mysql');
+const mysql = require("../config/mysql");
 
 class Contact {
-    constructor(businessID) {
-        this.businessID = businessID
-    }
+  constructor(businessID) {
+    this.businessID = businessID;
+  }
 
-    async save() {
+  async save() {}
 
-    }
+  static findByID(id) {}
 
-    static findByID(id) {
-
-    }
-
-    static findAll(businessID) {
-        let sql = `
-        (SELECT employee_id AS id, name, gender, CONCAT('employee') AS role, phone, address FROM employee WHERE employee.business_id = ${businessID})
+  static findAll(businessID) {
+    let sql = `
+        (SELECT employee_id AS id, name, gender, CONCAT('employee') AS role, phone, email FROM employee WHERE employee.business_id = ${businessID})
         UNION 
-        (SELECT customer_id AS id, name, gender, CONCAT('customer') AS role, phone, address FROM customer WHERE customer.business_id = ${businessID})
+        (SELECT customer_id AS id, name, gender, CONCAT('customer') AS role, phone, email FROM customer WHERE customer.business_id = ${businessID})
         UNION 
-        (SELECT vendor_id AS id, name, gender, CONCAT('vendor') AS role, phone, address FROM vendor WHERE vendor.business_id = ${businessID});`
-        return mysql.execute((sql))
-    }
+        (SELECT vendor_id AS id, name, gender, CONCAT('vendor') AS role, phone, email FROM vendor WHERE vendor.business_id = ${businessID});`;
+    return mysql.execute(sql);
+  }
 }
 
 module.exports = Contact;

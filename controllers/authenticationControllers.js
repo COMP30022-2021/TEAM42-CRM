fb4350ba58744c4f235b7896c3ff82bcb0f515e8
zelinwang528 <zelinw3@student.unimelb.edu.au>
2021-09-29 16:07:37 +0800

    feat_zelin: LINK-71 delete existing employee api
    Change-Id: I24d4230e68c8ed10f59deb64cbdbde6b12cef174

--- controllers/authenticationControllers.js
+++ controllers/authenticationControllers.js
@@ -129,3 +129,24 @@ exports.changePassword = async (req, res) => {
         })
     }
 }
+
+exports.deleteEmployee = async (req, res) => {
+    try {
+        const employeeId = req.params.id;
+        const newEmployee = new Authentication();
+
+        let result = newEmployee.deleteByID(employeeId);
+
+        console.log(result);
+        res.status(200).json({
+            status_code: 0,
+            status_message: "Success: Employee Delete",
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(200).json({
+            status_code: 400,
+            status_message: "Error: Internal Server Error"
+        })
+    }
+}
--- routes/authenticationRoutes.js
+++ routes/authenticationRoutes.js
@@ -3,10 +3,17 @@ const authenControllers = require("../controllers/authenticationControllers");
 const router = express.Router();
 
 // authentication routes
+
+// post login as an employee
 router.route("/login").post(authenControllers.login);
 
+// post create a new employee
 router.route("/register").post(authenControllers.register);
 
+// post change password
 router.route("/change/password").post(authenControllers.changePassword);
+
+// get to delete a existing employee
+router.route("/delete/:id").get(authenControllers.deleteEmployee);
 
 module.exports = router;

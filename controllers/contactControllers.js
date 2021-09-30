eec98909b30b2c8ae2a2f6ddf25c854d98417c09
zelinwang528 <zelinw3@student.unimelb.edu.au>
2021-09-30 10:52:59 +0800

    feat_zelin: LINK-73 add sort on get contacts api
    Change-Id: I527c78155a6fe91cf5d7476e7a717dabda11346c

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,13 +1,29 @@
 const Customer = require('../models/contact')
+const url = require('url');
 
 exports.getAllContact = async (req, res, next) => {
     try {
-        let [contacts, _] = await Customer.findAll(req.params.businessID);
-        res.status(200).json({
-            status_code: 200,
-            status_message: "Success",
-            contacts
-        })
+        let uriObj = url.parse(req.url, true)
+        if (uriObj.query.sort) {
+            let sort = uriObj.query.sort;
+            if (sort == "name") {
+                console.log(sort);
+                let [contacts, _] = await Customer.findAllWithSort(req.params.businessID, sort);
+                res.status(200).json({
+                    status_code: 200,
+                    status_message: "Success",
+                    sort: sort,
+                    contacts
+                })
+            }
+        } else {
+            let [contacts, _] = await Customer.findAll(req.params.businessID);
+            res.status(200).json({
+                status_code: 200,
+                status_message: "Success",
+                contacts
+            })
+        }
     } catch (err) {
         console.log(err);
         res.status(400).json({

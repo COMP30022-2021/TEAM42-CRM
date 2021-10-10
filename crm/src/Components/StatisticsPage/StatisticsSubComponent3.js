a3e3d2be8eeeb23eebac3c73e907dfef82d32697
VKASPERKOVIT <vkasperkovit@student.unimelb.edu.au>
2021-10-10 12:36:29 +1100

    Fixed formatting error

--- crm/src/Components/StatisticsPage/StatisticsSubComponent3.js
+++ crm/src/Components/StatisticsPage/StatisticsSubComponent3.js
@@ -8,6 +8,7 @@ export default function StatisticsSubComponent3({ left, top }) {
     third: 12000,
     foourth: 10000,
   };
+  
   return (
     <div className="statisticsDisplay" style={{ left: left, top: top }}>
       <Chart

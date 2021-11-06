29e0005d50153a18fd604a85639c597a98a898ee
vincentkasperkovitz <vkasperkovit@student.unimelb.edu.au>
2021-11-06 15:37:41 +1100

    Integration of statistics component

--- crm/src/Components/StatisticsPage/StatisticsSubComponent1.js
+++ crm/src/Components/StatisticsPage/StatisticsSubComponent1.js
@@ -1,30 +1,134 @@
 import React from "react";
 
 export default function StatisticsSubComponent1({ left, top }) {
-  const info = {
-    Revenue: 10000,
-    NumberItems: 350,
-    MostSold: "Coke",
-    LeastSold: "Fried chicken",
+  const [revenue, setRevenue] = React.useState("");
+  const [numberTransactions, setNumberTransactions] = React.useState("");
+  const [mostSold, setMostSold] = React.useState("");
+  const [leastSold, setLeastSold] = React.useState("");
+
+  const loadKeyStatistic = () => {
+    fetch(
+      "https://team42-crm.herokuapp.com/transaction/getTotalRevenueByDate/" +
+        localStorage.getItem("businessID"),
+      {
+        method: "post",
+        mode: "cors",
+        headers: new Headers({
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        }),
+        body: JSON.stringify({
+          start_date: "2000-1-1",
+          end_date: "2100-1-1",
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status_code === 200) {
+          console.log(data);
+          setRevenue(data.revenue[0]["SUM(total_price)"]);
+          console.log(data.revenue[0]["SUM(total_price)"]);
+        } else {
+          alert(data.status_message);
+        }
+      });
+
+    fetch(
+      "https://team42-crm.herokuapp.com/transaction/getNumberOfTransactions/" +
+        localStorage.getItem("businessID"),
+      {
+        method: "get",
+        mode: "cors",
+        headers: new Headers({
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status_code === 200) {
+          console.log(data);
+          setNumberTransactions(data.num[0].total_transactions);
+          console.log(data.num[0].total_transactions);
+        } else {
+          alert(data.status_message);
+        }
+      });
+
+    fetch(
+      "https://team42-crm.herokuapp.com/order/getAllSoldProducts" +
+        localStorage.getItem("businessID"),
+      {
+        method: "post",
+        mode: "cors",
+        headers: new Headers({
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        }),
+        body: JSON.stringify({
+          sortOrder: "DESC",
+          limit: 1,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status_code === 200) {
+          console.log(data);
+          setMostSold(data.products[0].product_id);
+          console.log(data.products[0].product_id);
+        } else {
+          alert(data.status_message);
+        }
+      });
+
+    fetch(
+      "https://team42-crm.herokuapp.com/order/getAllSoldProducts" +
+        localStorage.getItem("businessID"),
+      {
+        method: "post",
+        mode: "cors",
+        headers: new Headers({
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        }),
+        body: JSON.stringify({
+          sortOrder: "ASC",
+          limit: 1,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status_code === 200) {
+          console.log(data);
+          setLeastSold(data.products[0].product_id);
+          console.log(data.products[0].product_id);
+        } else {
+          alert(data.status_message);
+        }
+      });
   };
+
+  React.useEffect(() => {
+    loadKeyStatistic();
+  }, []);
+
   return (
     <div className="statisticsDisplay" style={{ left: left, top: top }}>
       <div className="chartTitle">Key Statistics</div>
 
-      <div className="pStatsDescription">Revenue generated: ${info.Revenue}</div>
+      <div className="pStatsDescription">Revenue generated: ${revenue}</div>
 
-      <div className="pStatsDescription">Number of Items Sold: {info.NumberItems}</div>
+      <div className="pStatsDescription">
+        Number of customer interactions: {numberTransactions}
+      </div>
 
-      <div className="pStatsDescription">Most sold item: {info.MostSold}</div>
+      <div className="pStatsDescription">Most sold item: {mostSold}</div>
 
-      <div className="pStatsDescription">Least sold item: {info.LeastSold}</div>
-
-      <button className="statButtons" style={{ top: "5%", left: "78%" }}>
-        <div style={{ color: "#ffffff" }}>Add Filters</div>
-      </button>
-      <button className="statButtons" style={{ top: "22%", left: "78%" }}>
-        <div style={{ color: "#ffffff" }}>Clear Filters</div>
-      </button>
+      <div className="pStatsDescription">Least sold item: {leastSold}</div>
     </div>
   );
 }

a7f8ac9c9dcab74a391b8ca10f84fec547d6a882
Yifan Guang <gyfff2000@gmail.com>
2021-11-12 19:59:33 +1100

    add api for adding one order entry

--- routes/orderDetailRoutes.js
+++ routes/orderDetailRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const orderDetailControllers = require("../controllers/orderDetailControllers");
 const router = express.Router();
 
+router.route("/create").post(orderDetailControllers.addProduct)
 router.route("/getProductQuarterlyRevenueInYear").post(orderDetailControllers.getProductQuarterlyRevenueInYear);
 router.route("/getAllSoldProducts:id").post(orderDetailControllers.getAllSoldProducts);
 
